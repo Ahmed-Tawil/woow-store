344f7eb8178a63681c6434b4259ce63ecd1506e1
ahmed tawil <ahmed0tawil@gmail.com>
2020-09-28 08:30:00 +0300

    last change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://admin:f5UZxPIZzQXCUg03@cluster0.3kn88.mongodb.net/woow-store?retryWrites=true&w=majority'
-    //MongoURI:'mongodb://localhost:27017/woow-store'
-
+    MongoURI:'mongodb://localhost:27017/woow-store'
 }
